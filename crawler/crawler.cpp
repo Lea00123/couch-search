5a54b2e1839960f81764ff309d62ac976347a5f8
lea.nareznoy <leanareznoy00123@gmail.com>
2024-08-25 19:01:45 +0300

    clowling config json + class clonfig  implementation

---
+++ crawler/crawler.cpp
@@ -0,0 +1,46 @@
+
+#include "crawler_config.h"
+
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using json = nlohmann::json;
+
+CrawlerConfig::CrawlerConfig(std::string const& a_config_file_path)
+{
+    std::ifstream config_file(a_config_file_path);
+    if (!config_file.is_open()) {
+        throw std::runtime_error("Could not open config file");
+    }
+
+    json data = json::parse(config_file);
+
+    m_start_urls = data["start_urls"].get<std::vector<std::string>>();
+    m_max_depth = data["max_depth"].get<int>();
+    m_max_limit_pages = data["max_pages"].get<int>();
+    m_crawling_mode = data["crawl_mode"].get<std::string>();
+}
+
+std::vector<std::string> const& CrawlerConfig::get_start_urls() const
+{
+    return m_start_urls;
+}
+
+int CrawlerConfig::get_max_depth() const
+{
+    return m_max_depth;
+}
+
+int CrawlerConfig::get_max_limit_pages() const
+{
+    return m_max_limit_pages;
+}
+
+std::string const& CrawlerConfig::get_crawling_mode() const
+{
+    return m_crawling_mode;
+}
+
---
+++ crawler/crawler_config.h
@@ -0,0 +1,44 @@
+#ifndef CRAWLER_CONFIG_H_
+#define CRAWLER_CONFIG_H_
+
+#include <string>
+#include <vector>
+
+class CrawlerConfig {
+public:
+public:
+    CrawlerConfig(std::string const& a_config_file_path);
+
+    /**
+     * Get the list of start URLs from the configuration.
+     * @return A constant reference to a vector of start URLs.
+     */
+    std::vector<std::string> const& get_start_urls() const;
+
+    /**
+     * Get the maximum number of pages to crawl.
+     * @return The maximum number of pages to crawl.
+     */
+    int get_max_limit_pages() const;
+
+    /**
+     * Get the maximum depth for crawling.
+     * @return The maximum depth for crawling.
+     */
+    int get_max_depth() const;
+
+    /**
+     * Get the crawling mode (BFS or DFS).
+     * @return A constant reference to the crawling mode string.
+     */
+    std::string const& get_crawling_mode() const;
+
+private:
+    std::vector<std::string> m_start_urls;
+    int m_max_limit_pages;
+    int m_max_depth;
+    std::string m_crawling_mode;
+
+};
+
+#endif  // CRAWLER_CONFIG_H_
